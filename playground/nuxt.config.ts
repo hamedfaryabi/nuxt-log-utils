11412276d265f0d8b60ffbdd04aee3c70b2d80fa
Hamed Faryabi <hamed.faryabi9@gmail.com>
2026-02-28 09:30:26 +0330

    chore: update playground configuration and app setup

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,5 +2,42 @@ export default defineNuxtConfig({
   modules: ['nuxt-log'],
   devtools: { enabled: true },
   compatibilityDate: 'latest',
-  // myModule: {},
+
+  runtimeConfig: {
+    public: {
+      logger: {
+        minLevel: 100,
+        output: ['console'],
+        includeMeta: true,
+
+        levels: {
+          400: {
+            output: ['console', 'file'],
+            filePath: 'logs/errors.log',
+            fileLogPeriod: 'daily',
+          },
+          500: {
+            output: ['console', 'api'],
+            apiUrl: '/api/logs',
+          },
+        },
+
+        loggers: {
+          auth: {
+            minLevel: 100,
+            mask: ['password', 'token'],
+          },
+          payment: {
+            output: ['console', 'file'],
+            filePath: 'logs/payment.log',
+            fileLogPeriod: 'monthly',
+            mask: {
+              cardNumber: (v: string) => `****${v.slice(-4)}`,
+              cvv: false,
+            },
+          },
+        },
+      },
+    },
+  },
 })
